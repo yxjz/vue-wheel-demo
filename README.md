4e0658e4f58e4fce18e20a37f5e36de809270cd1
yxjz <645633988@qq.com>
2018-06-28 16:10:25 +0800

    完成g-button的基本样式

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 作者 yxjz
 
+##安装
+本框架中css中border-box已开启
+---
+｛box-sizing:border-box｝
+---
+
 
---
+++ button.js
@@ -0,0 +1,8 @@
+
+/**
+ * Created by Administrator on 2018/6/28.
+ */
+Vue.component('g-button',{
+    template:`
+    <button class="g-button">按钮</button>`
+})
