a918a43aa5e400bdc545adfea911e036d4c76720
yxjz <645633988@qq.com>
2018-06-29 21:34:45 +0800

    添加icon和iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,22 +1,58 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 <script>
 
-   export default{
-
+    export default{
+        props: ['icon', 'iconPosition']
     }
 </script>
 <style lang="scss">
-    .g-button { font-size: var(--font-size); height: var(--button-height);
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
         padding: 0 1em;
-        border-radius: var(--border-radius); border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex; justify-content: center; align-items: center;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         vertical-align: middle;
-        &:hover { border-color: var(--border-color-hover); }
-        &:active { background-color: var(--button-active-bg); }
-        &:focus { outline: none; }
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .icon{
+            order: 1;
+            margin-right: .1em;
+        }
+        > .content{
+            order: 2;
+        }
+        &.icon-right{
+            > .icon{
+                order: 2;
+                margin-left: .1em;
+                margin-right: 0em;
+            }
+            > .content{
+                order: 1;
+            }
+        }
     }
 
 </style>
