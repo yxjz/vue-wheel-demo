31be66f2268ba3d31bf493a4ce0ec7424436378b
yxjz <645633988@qq.com>
2018-07-05 16:33:26 +0800

    安装chai-spies，使用chai,mock，写5个单元测试

--- src/app.js
+++ src/app.js
@@ -18,3 +18,96 @@ new Vue({
         loading3:false,
     }
 });
+
+import chai from 'chai'
+import spies from 'chai-spies';
+chai.use(spies);
+const expect = chai.expect;
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'settings'
+        }
+    });
+
+
+    button.$mount();
+    let userElement = button.$el.querySelector('use')
+    expect(userElement.getAttribute('xlink:href')).to.eq('#i-settings');
+    button.$el.remove();
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    });
+
+    button.$mount();
+    let userElement = button.$el.querySelector('use');
+    expect(userElement.getAttribute('xlink:href')).to.eq('#i-loading');
+    button.$el.remove();
+    button.$destroy()
+}
+
+
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'settings'
+
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let{order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    button.$el.remove();
+    button.$destroy()
+
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let{order} = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    button.$el.remove();
+    button.$destroy()
+}
+{
+
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+
+        }
+    });
+    vm.$mount();
+    let spy = chai.spy(function () {
+
+    });
+
+   vm.$on('click',spy);
+    let button  = vm.$el;
+    button.click();
+    expect(spy).to.have.been.called()
+}
